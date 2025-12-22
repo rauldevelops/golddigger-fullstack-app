9ce8487b36987db364026cf2e84c9a49e5ee5fee
Raul Develops <124627746+rauldevelops@users.noreply.github.com>
2025-12-22 12:49:34 -0500

    added CORS to res

--- utils/sendResponse.js
+++ utils/sendResponse.js
@@ -2,6 +2,9 @@ export function sendResponse(res, statusCode, contentType, payload) {
 
   res.statusCode = statusCode
   res.setHeader('Content-Type', contentType)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   res.end(payload)
 
 }
